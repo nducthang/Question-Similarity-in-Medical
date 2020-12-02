2e5b45ed9d364aa83aaa25ee6b76db7de0a22b7a
nducthang <nguyenducthang8a2@gmail.com>
2020-12-02 16:31:17 +0700

    custom nlu

---
+++ rasa/nlu/tokenizers/vi_tokenizer.py
@@ -0,0 +1,22 @@
+import re
+from typing import Any, Dict, List, Text
+from rasa.nlu.tokenizers.tokenizer import Token, Tokenizer
+from rasa.nlu.training_data import Message
+
+from rasa.nlu.constants import TOKENS_NAMES, MESSAGE_ATTRIBUTES
+# from underthesea import word_tokenize
+# from pyvi import word_tokenize
+from pyvi.ViTokenizer import tokenize
+
+class VietnameseTokenizer(Tokenizer):
+
+    provides = [TOKENS_NAMES[attribute] for attribute in MESSAGE_ATTRIBUTES]
+
+    def __init__(self, component_config: Dict[Text, Any] = None) -> None:
+        super().__init__(component_config)
+
+    def tokenize(self, message: Message, attribute: Text) -> List[Token]:
+        text = message.get(attribute)
+        words = tokenize(text)
+
+        return self._convert_words_to_tokens(words, text)
