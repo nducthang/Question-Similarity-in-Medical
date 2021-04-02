c39939db94c80720e1d652465f1da81736f0e9b7
nducthang <nguyenducthang8a2@gmail.com>
2021-04-02 15:52:45 +0700

    max width page

--- deepcare/TOOLS/app.py
+++ deepcare/TOOLS/app.py
@@ -2,6 +2,22 @@ import add_similary
 import statistical
 import streamlit as st
 
+
+def _max_width_():
+    max_width_str = f"max-width: 2000px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+_max_width_()
+
 PAGES = {
     "Thêm câu hỏi tương đồng": add_similary,
     "Thống kê": statistical
